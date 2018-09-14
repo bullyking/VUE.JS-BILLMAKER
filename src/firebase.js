4162de68e405936045b5113e6b26ea446fc36147
Kitja Rodrat <kitja.rodrat@gmail.com>
2018-09-14 12:36:46 +0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import {
 } from 'firebase';
 
 const app = initializeApp({
-  apiKey: "AIzaSyAtaSI4ZJscdsh_UinPT-XCzoiYsikSD2I",
-  authDomain: "billmaker-187a9.firebaseapp.com",
-  databaseURL: "https://billmaker-187a9.firebaseio.com",
-  projectId: "billmaker-187a9",
-  storageBucket: "billmaker-187a9.appspot.com",
-  messagingSenderId: "272113820685"
+  apiKey: "apiKey",
+  authDomain: "authDomain",
+  databaseURL: "databaseURL",
+  projectId: "projectId",
+  storageBucket: "storageBucket",
+  messagingSenderId: "messagingSenderId"
 });
 
 export const auth = app.auth();
